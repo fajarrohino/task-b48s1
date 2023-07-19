7a17a0ccc4fae0800dd06e3c7b83e1bde11ab12d
Fajar Rohino <fajarrohino14@gmial.com>
2023-07-19 20:22:37 +0700

    basic go

---
+++ learn.go
@@ -0,0 +1,66 @@
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	// variable
+// 	// tipe data int
+// 	// var x int
+// 	// fmt.Println(x)
+// 	// output 0
+// 	// because variables declared without corrasponding inititalization are zero value
+// 	// zero value { int -> 0, steing = "" -> (output empty), bool = false -> 0 }
+
+// 	// string
+// 	// var y string =`Hy i'm Ino`
+// 	// fmt.Println(y)
+// 	// bisa menggunakan petik dua atau backtick
+
+// 	//  bool
+// 	// var e bool = false
+// 	// fmt.Println(e)
+
+// 	// //  float64
+// 	// var a float64 = 8.4
+// 	// fmt.Println(a)
+
+// 	// syntax is shorthand
+// 	// e := "I'm engineer programmer development"
+// 	// fmt.Println(e)
+
+// 	// operator
+// 	// v := 2 + 8
+// 	// e := v != 10
+// 	// fmt.Println("hasil = ", v, e)
+
+// 	// operator logic
+// 	// t :=true
+// 	// f := false
+// 	// e := t||f
+// 	// fmt.Println(e)
+
+// 	// array
+// 	// fix array
+// 	// a := [1,3,4,5]
+// 	// fmt.Println(a)
+// 	// arr:=[]string{"fajar","rohino","ino"}
+// 	// fmt.Println(arr)
+
+// 	// dinamis array
+// 	// arr :=[]int{1,2,3,4}
+// 	// arr = append(arr, 19)
+// 	// fmt.Println(arr)
+
+// 	// percabangan
+// 	// buat percabangan nilai d <=70 c = 71-80, b = 81-90, a =91-100
+// 	v := 99
+// 	if v <= 70 {
+// 		fmt.Println("Nilai D")
+// 	} else if v <= 80 {
+// 		fmt.Println("Nilai C")
+// 	} else if v <= 90 {
+// 		fmt.Println("Nilai B")
+// 	} else {
+// 		fmt.Println("Nilai A")
+// 	}
+// }
